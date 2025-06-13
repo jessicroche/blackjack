239718a456c01b39b795cccef514ab77bccd2d60
jessicroche <jessicaburitto@gmail.com>
2025-06-13 15:31:47 -0300

    alteracao estrutura da top level entity, add saidas de sumDecimal

--- codigos em VHDL/implementacao.vhd
+++ codigos em VHDL/implementacao.vhd
@@ -3,48 +3,55 @@ use ieee.std_logic_1164.all;
 
 entity implementacao is
     port (
-        clk     : in  std_logic; -- Clock signal
-        start   : in  std_logic; -- Reset signal      
-        hit    : in  std_logic; -- Hit signal
-        stay    : in  std_logic; -- Stay signal
-        reqCarta : in  std_logic; -- Request for a card
-        reqManual : in  std_logic; -- Request for manual card input
-        cartaManual : in  std_logic_vector(3 downto 0); -- Manual card input
-        Pwin : out std_logic; -- Player win signal
-        Plose : out std_logic; -- Player lose signal
-        TIE : out std_logic; -- Tie signal
-        hexCard : out std_logic_vector(7 downto 0);
-        sum1_dec : out std_logic_vector(7 downto 0);
-        sum2_dec : out std_logic_vector(7 downto 0)
-     );
+        clk         : in  std_logic;
+        start       : in  std_logic;
+        hit         : in  std_logic;
+        stay        : in  std_logic;
+        reqManual   : in  std_logic;
+        cartaManual : in  std_logic_vector(3 downto 0);
+        Pwin        : out std_logic;
+        Plose       : out std_logic;
+        TIE         : out std_logic;
+        hex_card    : out std_logic_vector(7 downto 0);
+        sum1_dec    : out std_logic_vector(7 downto 0);
+        sum2_dec    : out std_logic_vector(7 downto 0)
+    );
 end implementacao;
 
 architecture Behavioral of implementacao is
-    signal cards_req : std_logic; -- Card request signal
-    signal card_reset : std_logic; -- Card reset signal
-    begin
-        cards_inst : entity work.cards
-            port map (
-                clk => clk,
-                reset => start,
-                reqCarta => cards_req,
-                reqManual => reqManual,
-                cartaManual => cartaManual,
-                random_number => hexCard
-            );
-        blackjack_inst : entity work.blackjack
-            port map (
-                HIT => hit,
-                STAY => stay,
-                START => start,
-                CLK => clk,
-                CARD => hexCard,
-                REQCARD => cards_req,
-                PWIN => Pwin,
-                PLOSE => Plose,
-                TIE => TIE,
-                hexCard => hexCard,
-                sumDigit1 => sum1_dec,
-                sumDigit2 => sum2_dec
-            );
+
+    signal cards_req   : std_logic;
+    signal hexCard_int : std_logic_vector(3 downto 0);
+    signal sum1_int    : std_logic_vector(7 downto 0);
+    signal sum2_int    : std_logic_vector(7 downto 0);
+begin
+    cards_inst : entity work.cards
+        port map (
+            clk         => clk,
+            reset       => start,
+            reqCarta    => cards_req,
+            reqManual   => reqManual,
+            cartaManual => cartaManual,
+            random_number => hexCard_int
+        );
+
+    blackjack_inst : entity work.blackjack
+        port map (
+            HIT        => hit,
+            STAY       => stay,
+            START      => start,
+            CLK        => clk,
+            CARD       => hexCard_int,
+            REQCARD    => cards_req,
+            PWIN       => Pwin,
+            PLOSE      => Plose,
+            TIE        => TIE,
+            hexCard    => hex_card,
+            sumDigit1  => sum1_int,
+            sumDigit2  => sum2_int
+        );
+
+    hex_card  <= hexCard_int;
+    sum1_dec  <= sum1_int;
+    sum2_dec  <= sum2_int;
 end Behavioral;
