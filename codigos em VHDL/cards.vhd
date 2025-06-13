c0954b8aaa56892fd07c99551d789fb11a4b6ef6
jessicroche <jessicaburitto@gmail.com>
2025-06-13 16:43:12 -0300

    alteracao na logica de gerar cartas

--- codigos em VHDL/cards.vhd
+++ codigos em VHDL/cards.vhd
@@ -9,7 +9,7 @@ entity cards is
         reqCarta : in  std_logic; 
         reqManual : in  std_logic; 
         cartaManual : in  std_logic_vector(3 downto 0);
-        random_number : out std_logic_vector(3 downto 0) 
+        cartaFinal : out std_logic_vector(3 downto 0) 
     );
 end cards;
 
@@ -19,68 +19,40 @@ architecture Behavioral of cards is
     signal next_state    : state_type;
     signal lfsr : std_logic_vector(15 downto 0) := "1010110010100000";  -- SEED
     signal rnd_int : integer range 1 to 52; 
-    signal cartaGerada : std_logic_vector(3 downto 0); 
-    signal lfsrenable : std_logic; 
+    signal random_number, cartaGerada : std_logic_vector(3 downto 0);
 begin
 
-    process(current_state, reqCarta, reqManual, cartaManual,rnd_int)
-    begin
-        next_state <= current_state; 
-        cartaGerada <= (others => '0');
-        lfsrenable <= '0'; 
-        case current_state is
-            when idle =>
-                if reqCarta = '1' and reqManual='0' then
-                    next_state <= rand;
-                    lfsrenable <= '1'; 
-                elsif reqCarta='1' and reqManual = '1' then
-                    next_state <= manual;
-                    lfsrenable <= '0';  
-                else
-                    next_state <= idle; 
-                end if;
-
-            when rand =>
-                next_state <= read;
-                cartaGerada <= std_logic_vector(to_unsigned(rnd_int, 4)); 
-
-            when read =>
-                if reqManual = '0' then 
-                    cartaGerada <= std_logic_vector(to_unsigned(rnd_int, 4));
-                else 
-                    cartaGerada <= cartaManual;
-                end if;
-                if reqCarta = '0' then
-                    next_state <= idle; 
-                else
-                    next_state <= read;
-                end if;
-
-            when manual =>
-                cartaGerada <= cartaManual; 
-                next_state <= read;
-
-            when others =>
-                next_state <= idle; 
-
-        end case;
-    end process;
-
-    rnd_int <= (to_integer(unsigned(lfsr)) mod 13)+1; -- Generate a number between 1 and 13
     process(clk, reset)
     begin
         if reset = '1' then
-            lfsr <= "1110110010101110";
+            lfsr <= "1110110010101110"; -- volta pra seed original
             current_state <= idle;
         elsif falling_edge(clk) then
+            case current_state is
+            when idle =>
+                if reqCarta = '1' and reqManual='0' then
+                    next_state <= rand;
+                elsif reqCarta='1' and reqManual = '1' then
+                    next_state <= manual;
+                else
+                    next_state <= idle; 
+                end if;
+            end case;
             current_state <= next_state;
-            if lfsrenable = '1' then
-                lfsr <= lfsr(14 downto 0) & 
-                (lfsr(15) xor lfsr(13) xor lfsr(12) xor lfsr(10));
-            end if;
         end if;
     end process;
-
-    random_number <= cartaGerada;
     
+    process(current_state)
+    begin
+        case current_state is 
+            when rand =>
+                lfsr <= lfsr(14 downto 0) & 
+                (lfsr(15) xor lfsr(13) xor lfsr(12) xor lfsr(10));
+                rnd_int <= (to_integer(unsigned(lfsr)) mod 13) + 1;
+                random_number <= std_logic_vector(to_unsigned(rnd_int, 4));
+            when manual =>
+                cartaFinal <= cartaManual; 
+        end case;
+    end process;
+
 end Behavioral;
