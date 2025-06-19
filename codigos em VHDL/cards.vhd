db53d84979d1e656832d3bb471cf605a985b0724
jessicroche <jessicaburitto@gmail.com>
2025-06-18 21:12:36 -0300

    correcao na atribuicao da carta manual, mudanca no estado de read

--- codigos em VHDL/cards.vhd
+++ codigos em VHDL/cards.vhd
@@ -14,9 +14,8 @@ entity cards is
 end cards;
 
 architecture Behavioral of cards is
-    type state_type is (idle, rand, manual);
+    type state_type is (idle, geraCarta,leCarta);
     signal current_state : state_type; 
-    signal next_state    : state_type;
     signal lfsr : std_logic_vector(15 downto 0) := "1010110010100000";  -- SEED
     signal rnd_int : integer range 1 to 52; 
     signal random_number, cartaGerada : std_logic_vector(3 downto 0);
@@ -30,35 +29,41 @@ begin
         elsif falling_edge(clk) then
             case current_state is
             when idle =>
-                if reqCarta = '1' and reqManual='0' then
-                    next_state <= rand;
-                elsif reqCarta='1' and reqManual = '1' then
-                    next_state <= manual;
+                if reqCarta = '1' then
+                    current_state <= geraCarta;
                 else
-                    next_state <= idle; 
+                    current_state <= idle; 
                 end if;
-            when rand => -- add transicao de rand de volta para idle
-                lfsr <= lfsr(14 downto 0) & 
-                (lfsr(15) xor lfsr(13) xor lfsr(12) xor lfsr(10));
-                    next_state <= idle;
-            when manual => --add transicao de manual de volta para idle
-                    next_state <= idle; 
+            when geraCarta => -- add transicao de rand de volta para idle
+                if reqManual = '0' then 
+                    lfsr <= lfsr(14 downto 0) & 
+                    (lfsr(15) xor lfsr(13) xor lfsr(12) xor lfsr(10));
+                    rnd_int <= (to_integer(unsigned(lfsr)) mod 13) + 1;
+                    random_number <= std_logic_vector(to_unsigned(rnd_int, 4));
+                end if;
+                    current_state <= leCarta;
+                when leCarta =>
+                    current_state <= idle;
 				when others => null;
             end case;
-            current_state <= next_state;
         end if;
     end process;
     
     process(current_state)
     begin
         case current_state is 
-            when rand =>
-                rnd_int <= (to_integer(unsigned(lfsr)) mod 13) + 1;
-                random_number <= std_logic_vector(to_unsigned(rnd_int, 4));
-                cartaFinal <= random_number; --correcao da atribuicao da carta na saida
-            when manual =>
-                cartaFinal <= cartaManual;
-				when others => null;
+            when idle =>
+                cartaFinal <= (others => '0'); --correcao da atribuicao da carta na saida
+            when geraCarta =>
+                if reqManual = '0' then
+                    cartaFinal <= random_number;
+                else
+                    cartaFinal(0) <= cartaManual(0);
+                    cartaFinal(1) <= cartaManual(1);
+                    cartaFinal(2) <= cartaManual(2);
+                    cartaFinal(3) <= cartaManual(3);                    
+                end if;
+			when others => null;
         end case;
     end process;
 
